c3f758488a6481dcc243f80857ac6c2914998f3d
Rati Gelashvili <gelash@aptoslabs.com>
2022-11-17 02:57:36 -0800

    Batch Aggregator and tests

--- consensus/src/quorum_store/mod.rs
+++ consensus/src/quorum_store/mod.rs
@@ -4,7 +4,10 @@
 /// Equivalent to directly fetching blocks from mempool without a quorum store.
 pub mod direct_mempool_quorum_store;
 
+mod batch_aggregator;
 pub(crate) mod batch_reader;
+mod types;
+
 mod counters;
 #[cfg(test)]
 mod tests;
